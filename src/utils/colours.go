8b9ad7e549c162e41741b9ccb80193e314cb4218
Connor Wright <connor@connorwright.uk>
2019-04-24 16:01:02 +0100

    add embed colours

---
+++ src/utils/colours.go
@@ -0,0 +1,17 @@
+package utils
+
+/**
+
+ * Created by cxnky on 24/04/2019 at 15:51
+ * utils
+ * https://github.com/cxnky/
+
+**/
+
+func GetInformationColour() int {
+	return 0x00BFFF
+}
+
+func GetErrorColour() int {
+	return 0xFF0000
+}
